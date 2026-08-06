773017c42a96f4a0ceacae521d8e284b4c336b20
LiLQD <luuquangdung1511@gmail.com>
2026-08-06 21:03:57 +0700

    refactor: simplify Holodex API fetching

--- src/Cards.js
+++ src/Cards.js
@@ -1,21 +1,10 @@
 const apiKey = import.meta.env.VITE_HOLODEX_API_KEY;
 console.log(apiKey);
-const res = await fetch(
-  "https://holodex.net/api/v2/channels?org=Hololive&type=vtuber&limit=80",
+const channels = await fetch(
+  "https://holodex.net/api/v2/channels?org=Hololive&limit=12",
   { headers: { "X-APIKEY": apiKey } },
-);
-const all = await res.json();
-
-function shuffle(arr) {
-  const a = [...arr];
-  for (let i = a.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-    [a[i], a[j]] = [a[j], a[i]];
-  }
-  return a;
-}
-const twelve = shuffle(all).slice(0, 12);
-export const cardList = twelve.map((v) => ({
-  name: v.english_name || v.name,
-  photo: v.photo,
+).then((res) => res.json());
+export const cardList = channels.map((channel) => ({
+  name: channel.english_name || channel.name,
+  photo: channel.photo,
 }));
