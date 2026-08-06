4c8e9a12b62fd880de186068c7f8785547f31387
LiLQD <luuquangdung1511@gmail.com>
2026-08-06 21:52:45 +0700

    fix: set API limit to 100

--- src/Cards.js
+++ src/Cards.js
@@ -1,11 +1,11 @@
 const apiKey = import.meta.env.VITE_HOLODEX_API_KEY;
 console.log(apiKey);
 const channels = await fetch(
-  "https://holodex.net/api/v2/channels?org=Hololive&type=vtuber&limit=50",
+  "https://holodex.net/api/v2/channels?org=Hololive&type=vtuber&limit=100",
   { headers: { "X-APIKEY": apiKey } },
 ).then((res) => res.json());
 const excludedNames = ["hololive Dreams", "UNIT B", "HOLOEARTH"];
-
+console.log("Fetched data: ", channels);
 const shuffled = channels
   .filter(
     (channel) =>
