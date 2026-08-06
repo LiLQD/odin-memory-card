862dcb6a2aa0e21b236c95af9c5ff1749590334f
LiLQD <luuquangdung1511@gmail.com>
2026-08-06 21:51:09 +0700

    feat: add filter to remove offcial channels from list and randomly take 12 vtubers

--- src/Cards.js
+++ src/Cards.js
@@ -1,10 +1,26 @@
 const apiKey = import.meta.env.VITE_HOLODEX_API_KEY;
 console.log(apiKey);
 const channels = await fetch(
-  "https://holodex.net/api/v2/channels?org=Hololive&limit=12",
+  "https://holodex.net/api/v2/channels?org=Hololive&type=vtuber&limit=50",
   { headers: { "X-APIKEY": apiKey } },
 ).then((res) => res.json());
-export const cardList = channels.map((channel) => ({
-  name: channel.english_name || channel.name,
-  photo: channel.photo,
-}));
+const excludedNames = ["hololive Dreams", "UNIT B", "HOLOEARTH"];
+
+const shuffled = channels
+  .filter(
+    (channel) =>
+      !channel.inactive &&
+      channel.group !== "Official" &&
+      !excludedNames.some((name) => channel.name.startsWith(name)),
+  )
+  .map(({ english_name, photo }) => ({
+    english_name,
+    photo,
+  }));
+
+for (let i = shuffled.length - 1; i > 0; i--) {
+  const j = Math.floor(Math.random() * (i + 1));
+  [shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]];
+}
+
+export const cardList = shuffled.slice(0, 12);
