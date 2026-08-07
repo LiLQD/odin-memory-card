e07084a8d94afc0f1e636861e6831535d6aba4da
LiLQD <luuquangdung1511@gmail.com>
2026-08-07 13:05:22 +0700

    fix: add more filter for sub channel

--- src/Cards.js
+++ src/Cards.js
@@ -4,23 +4,41 @@ const channels = await fetch(
   "https://holodex.net/api/v2/channels?org=Hololive&type=vtuber&limit=100",
   { headers: { "X-APIKEY": apiKey } },
 ).then((res) => res.json());
-const excludedNames = ["hololive Dreams", "UNIT B", "HOLOEARTH"];
-console.log("Fetched data: ", channels);
+const excludedNames = [
+  "hololive Dreams",
+  "UNIT B",
+  "HOLOEARTH",
+  "ACHRORA",
+  "Midnight Grand Orchestra",
+  "hololive OFFICIAL CARD GAME",
+  "Octavio",
+];
+const isSubChannel = (channel) => {
+  const name = (channel.english_name || channel.name || "").toLowerCase();
+  return name.includes("sub") || name.includes("subch");
+};
 const shuffled = channels
   .filter(
     (channel) =>
       !channel.inactive &&
+      !isSubChannel(channel) &&
       channel.group !== "Official" &&
-      !excludedNames.some((name) => channel.name.startsWith(name)),
+      !excludedNames.some((name) =>
+        (channel.english_name || channel.name || "")
+          .toLowerCase()
+          .includes(name.toLowerCase()),
+      ),
   )
   .map(({ english_name, photo }) => ({
     english_name,
     photo,
   }));
-
-for (let i = shuffled.length - 1; i > 0; i--) {
-  const j = Math.floor(Math.random() * (i + 1));
-  [shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]];
+console.log("Filtered data: ", shuffled);
+export function fisherYatesShuffle(arr) {
+  for (let i = arr.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [arr[i], arr[j]] = [arr[j], arr[i]];
+  }
 }
-
+fisherYatesShuffle(shuffled);
 export const cardList = shuffled.slice(0, 12);
