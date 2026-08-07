ec1038dee69464430ce3018da431286c610cc39c
LiLQD <luuquangdung1511@gmail.com>
2026-08-07 13:51:48 +0700

    feat: add Card and CardList components

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
+import { cardList, fisherYatesShuffle } from "./Cards";
 import "./App.css";
-
 function App() {
   const [currentScore, setCurrentScore] = useState(0);
   const [bestScore, setBestScore] = useState(0);
   return (
     <>
       <Header currentScore={currentScore} bestScore={bestScore}></Header>
+      <CardList
+        currentScore={currentScore}
+        bestScore={bestScore}
+        setCurrentScore={setCurrentScore}
+        setBestScore={setBestScore}
+      />
     </>
   );
 }
@@ -27,5 +33,39 @@ function Header({ currentScore, bestScore }) {
       </div>
     </div>
   );
+}
+function Card({ name, photo, handleClick }) {
+  return (
+    <div className="card" onClick={handleClick}>
+      <img src={photo} alt={name + "'s photo"} />
+      <p className="name">{name}</p>
+    </div>
+  );
+}
+function CardList({ currentScore, bestScore, setCurrentScore, setBestScore }) {
+  const [selected, setSelected] = useState([]);
+  const [cards, setCards] = useState(cardList);
+  const listItems = cards.map((card) => (
+    <Card
+      name={card.english_name}
+      photo={card.photo}
+      handleClick={() => handleCardClick(card.id)}
+      key={card.id}
+    />
+  ));
+  function handleCardClick(id) {
+    if (!selected.includes(id)) {
+      setSelected([...selected, id]);
+      const score = currentScore + 1;
+      if (score > bestScore) setBestScore(score);
+      setCurrentScore(score);
+      setCards(fisherYatesShuffle(cards));
+    } else {
+      setCurrentScore(0);
+      setSelected([]);
+      setCards(fisherYatesShuffle(cards));
+    }
+  }
+  return <div className="card-list">{listItems}</div>;
 }
 export default App;
